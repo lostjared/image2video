744c509d70c1e78c05979877e0f71f14b1403542
lostjared <jaredbruni@gmail.com>
2018-09-30 16:24:09 -0700

    started working on checking if the info inputted is valid

--- image2video/image2video/Controller.h
+++ image2video/image2video/Controller.h
@@ -1,20 +1,17 @@
 
-#import<Foundation/Foundation.h>
 #import<Cocoa/Cocoa.h>
-#undef check
-#include<opencv2/videoio.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/highgui.hpp>
-
 
 @interface Controller : NSObject {
     IBOutlet NSButton *add_files, *remove_file, *move_file_up, *move_file_down, *build_video, *stretch_video;
     IBOutlet NSTableView *table_view;
     IBOutlet NSTextView  *text_log;
     IBOutlet NSTextField *field_fps, *field_w, *field_h;
+    double fps_value, width_value, height_value;
 }
 
 - (IBAction) buildVideo: (id) sender;
+- (BOOL) checkInput;
 
 @end
 
+NSInteger _NSRunAlertPanel(NSString *msg1, NSString *msg2, NSString *button1, NSString *button2, NSString *button3);
--- image2video/image2video/Controller.mm
+++ image2video/image2video/Controller.mm
@@ -1,12 +1,53 @@
 
+#import<Cocoa/Cocoa.h>
+#undef check
+
+#include<opencv2/videoio.hpp>
 #import "Controller.h"
+
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
 
 @implementation Controller
 
 - (IBAction) buildVideo: (id) sender {
     
+    fps_value = width_value = height_value = 0;
+    if([self checkInput] == NO)
+        return;
+    
     cv::VideoWriter writer;
     
 }
 
+- (BOOL) checkInput {
+    fps_value = atof([[field_fps stringValue] UTF8String]);
+    if(fps_value<= 0 || fps_value > 60) {
+        _NSRunAlertPanel(@"FPS value is incorrect", @"Please use a valid frames per second value", @"Ok", nil, nil);
+        return NO;
+    }
+    width_value = atof([[field_w stringValue] UTF8String]);
+    if(width_value <= 0 || width_value > 3840) {
+        _NSRunAlertPanel(@"Enter valid frame width", @"Please use a valid frame width variable", @"Ok", nil, nil);
+        return NO;
+    }
+    height_value = atof([[field_h stringValue] UTF8String]);
+    if(height_value <= 0 || width_value > 2160) {
+        _NSRunAlertPanel(@"Enter valid frame height", @"Please use a valid frame height variable", @"Ok", nil, nil);
+        return NO;
+    }
+    return YES;
+}
+
 @end
+
+NSInteger _NSRunAlertPanel(NSString *msg1, NSString *msg2, NSString *button1, NSString *button2, NSString *button3) {
+    NSAlert *alert = [[NSAlert alloc] init];
+    if(button1 != nil) [alert addButtonWithTitle:button1];
+    if(button2 != nil) [alert addButtonWithTitle:button2];
+    if(msg1 != nil) [alert setMessageText:msg1];
+    if(msg2 != nil) [alert setInformativeText:msg2];
+    NSInteger rt_val = [alert runModal];
+    return rt_val;
+}
