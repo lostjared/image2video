fe535f9fc6a0e3aab61c2f0d75bfcfbcf1120aa0
lostjared <jaredbruni@gmail.com>
2018-09-30 14:59:43 -0700

    connected interface controls to Controller object

--- image2video/image2video/Controller.h
+++ image2video/image2video/Controller.h
@@ -1,5 +1,6 @@
 
 #import<Foundation/Foundation.h>
+#import<Cocoa/Cocoa.h>
 #undef check
 #include<opencv2/videoio.hpp>
 #include<opencv2/imgproc.hpp>
@@ -7,10 +8,13 @@
 
 
 @interface Controller : NSObject {
-    
-    
+    IBOutlet NSButton *add_files, *remove_file, *move_file_up, *move_file_down, *build_video, *stretch_video;
+    IBOutlet NSTableView *table_view;
+    IBOutlet NSTextView  *text_log;
+    IBOutlet NSTextField *field_fps, *field_w, *field_h;
 }
 
 - (IBAction) buildVideo: (id) sender;
 
 @end
+
